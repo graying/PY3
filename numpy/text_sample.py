482b67d38e0bb0c59057938a29f8523ed8410b02
graying <graying@gmail.com>
2017-09-07 20:32:10 -0700

    update from Arch linux ...

--- numpy/text_sample.py
+++ numpy/text_sample.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(1010101010)
+np.random.seed(101010)
 
 mu, sigma = 100, 15
 x = mu + sigma * np.random.randn(10000)
 
 n, bins, patches = plt.hist(x, 50, normed=1, facecolor='g', alpha=0.55)
 
-plt.xlable('Smarts')
-plt.ylable('Probability')
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
 plt.title('Histogram of IQ')
-plt.text(60,0.025, r`$\mu=100,\ \sigma=15$`)
+plt.text(60,0.025, r'$\mu=100,\ \sigma=15$')
 plt.axis([40,160,0,0.03])
 plt.grid(True)
 plt.show()
