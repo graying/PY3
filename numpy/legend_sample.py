99918702d7c658af9264edc3a3739e780843039c
graying <graying@gmail.com>
2017-09-09 21:13:45 -0700

    add some samples

--- numpy/legend_sample.py
+++ numpy/legend_sample.py
@@ -1,16 +1,23 @@
-import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 import matplotlib.patches as mpatches
-line_up = plt.plot([1,2,3], label='Line up', color="black")
-line_dw = plt.plot([3,2,1], label='line dw')
+import matplotlib.pyplot as plt
+
+plt.xlim(-3, 3)
+plt.ylim(0, 3)
+
+line_up = plt.plot([1, 2, 3], label='Line up', color="black")
+line_dw = plt.plot([3, 2, 1], label='line dw')
+line_blue = plt.plot([2, 2, 2], '*', color='blue')
 # plt.legend() can bring the legend on
 # plt.legend()
 
 # customize the legends
 red_patch = mpatches.Patch(color='red', label='The red data')
 grn_patch = mpatches.Patch(color='green', label='The grn data')
+blue_line = mlines.Line2D([], [], color='blue', marker='*',
+                          markersize=15, label='Blue Starts')
 
-plt.legend(handles=[red_patch, grn_patch])
-
+plt.legend(handles=[red_patch, grn_patch, blue_line])
 
 plt.grid()
 plt.show()
---
+++ numpy/transfermation_sample.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = np.arange(1, 100)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(a)
+ax.set_xlim(-100, 100)
+ax.set_ylim(0, 100)
+
+# ax.transData.transform((10, 1)) transfer the data to display coordinate
+
+print(ax.transData.transform((10, 1)))
+
+print(ax.transData.transform((-10, 1)))
+
+plt.show()
