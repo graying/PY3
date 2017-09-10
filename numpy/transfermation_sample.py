7573a80894751dc8977d955a3e81f8f528a22430
graying <graying@gmail.com>
2017-09-09 22:27:42 -0700

    add some samples

--- numpy/transfermation_sample.py
+++ numpy/transfermation_sample.py
@@ -6,7 +6,7 @@ a = np.arange(1, 100)
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(a)
-ax.set_xlim(-100, 100)
+ax.set_xlim(0, 100)
 ax.set_ylim(0, 100)
 
 # ax.transData.transform((10, 1)) transfer the data to display coordinate
