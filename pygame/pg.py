7b16bdd7b50600cdb3ac8759b4b5d53dc5424149
graying <graying@gmail.com>
2018-12-25 16:09:25 +0800

    good

--- pygame/pg.py
+++ pygame/pg.py
@@ -4,7 +4,9 @@ from pygame.locals import *
 pygame.init()
 dis = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Hello Pygamer...")
+pygame.draw.line(dis, (255,0,255), (0,0), (800,600), 4)
 while True:
+	pygame.display.update()
 	for event in pygame.event.get():
 		if event.type==QUIT:
 			pygame.quit()
