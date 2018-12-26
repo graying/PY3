b4ed90ef64ba8e03d6db3bbf34c6e46759294d89
graying <graying@gmail.com>
2018-12-26 22:43:49 +0800

    update

--- pygame/pg_draw.py
+++ pygame/pg_draw.py
@@ -7,12 +7,18 @@ pygame.display.set_caption("Hello Pygamer...")
 def rndColor():
     return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-dis.fill(rndColor())
-pygame.draw.line(dis, rndColor(), (0,0), (800,600), 4)
-pygame.draw.line(dis, rndColor(), (800,0), (0,600), 5)
+def draw_it():
+    dis.fill(rndColor())
+    pygame.draw.line(dis, rndColor(), (0,0), (800,600), 4)
+    pygame.draw.line(dis, rndColor(), (800,0), (0,600), 5)
+
+draw_it()
+
 while True:
-	pygame.display.update()
-	for event in pygame.event.get():
-		if event.type==QUIT:
-			pygame.quit()
-			sys.exit()
+    pygame.display.update()
+    for event in pygame.event.get():
+        if (event.type == QUIT):
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYUP:
+            draw_it()
