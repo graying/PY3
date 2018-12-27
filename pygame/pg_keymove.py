ec107f7a06e80fa626ba43682e8a44773881cc93
graying <graying@gmail.com>
2018-12-27 23:05:36 +0800

    updated

--- pygame/pg_keymove.py
+++ pygame/pg_keymove.py
@@ -1,32 +1,55 @@
 import pygame
 pygame.init()
 
-dis = pygame.display.set_mode((500,500))
+dis_x = 400
+dis_y = 400
+
+dis = pygame.display.set_mode((dis_x,dis_y))
 pygame.display.set_caption("PYgamer...")
 
-x,y = 50,50
+
+x,y = 50,250
 width = 40
 height = 60
-vel = 5
+vel = 3
 
+isJump = False
+jumpCount = 10
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < dis_x-width-vel:
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
 
+    if (not isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < dis_y-height-vel:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+            #print("space pressed isJump is:",isJump)
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+            print ("in isJump x and y is:", x, y)
+        else:
+            isJump = False
+            jumpCount = 10
+                
     dis.fill((0,0,0))
     pygame.draw.rect(dis, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
