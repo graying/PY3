dc7053f1e6fc8fbf38aff3915ec5c193cf8e4a71
graying <graying@gmail.com>
2019-01-01 21:46:12 +0800

    update py_draw

--- pygame/pg_draw.py
+++ pygame/pg_draw.py
@@ -1,24 +1,30 @@
-import pygame, sys, random
+import sys
+
+import pygame
+import random
 from pygame.locals import *
 
 pygame.init()
-dis = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Hello Pygamer...")
+dis = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Hello PYGamer...")
 
-def rndColor():
-    return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+def rndcolor():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
 
 def draw_it():
-    dis.fill(rndColor())
-    pygame.draw.line(dis, rndColor(), (0,0), (800,600), 4)
-    pygame.draw.line(dis, rndColor(), (800,0), (0,600), 5)
+    dis.fill(rndcolor())
+    pygame.draw.line(dis, rndcolor(), (0, 0), (800, 600), 4)
+    pygame.draw.line(dis, rndcolor(), (800, 0), (0, 600), 5)
+
 
 draw_it()
 
 while True:
     pygame.display.update()
     for event in pygame.event.get():
-        if (event.type == QUIT):
+        if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == KEYUP:
