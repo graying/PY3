cdbda52cd4bac025d8be2ad7ac471384fc659d97
graying <graying@gmail.com>
2019-01-29 21:39:32 +0800

    update format of cal_24 files

--- tk/Calculate_24_P3.py
+++ tk/Calculate_24_P3.py
@@ -5,28 +5,28 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import tkinter as tk
 import sys
+import tkinter as tk
 
-win=tk.Tk()
+win = tk.Tk()
 win.title("calculate 24")
 win.geometry("800x600")
 
 
 def on_quit():
-#    if tk.messagebox.askokcancel("Quiting", "please comfirm your action"):
+    #    if tk.messagebox.askokcancel("Quiting", "please comfirm your action"):
     win.destroy()
     sys.exit()
-        
+
+
 mainmenu = tk.Menu(win)
 filemenu = tk.Menu(mainmenu, tearoff=0)
 
-#filemenu.add_command(label='Add', command=on_btn_add)
-#filemenu.add_command(label='Clear', command=on_btn_clear)
+# filemenu.add_command(label='Add', command=on_btn_add)
+# filemenu.add_command(label='Clear', command=on_btn_clear)
 filemenu.add_separator()
 filemenu.add_command(label='Quit', command=on_quit)
 mainmenu.add_cascade(label="File", menu=filemenu)
 win.config(menu=mainmenu)
-
 
 win.mainloop()
