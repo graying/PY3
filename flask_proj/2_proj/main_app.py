c9ddec90d7ecb5451ee9485f041c943a73fb6dc0
graying <graying@gmail.com>
2019-02-15 00:26:52 +0800

    update...

--- flask_proj/2_proj/main_app.py
+++ flask_proj/2_proj/main_app.py
@@ -1,10 +1,12 @@
 import random
+import sqlite3
 
-from flask import Flask, render_template, flash
+from flask import g, Flask, render_template, flash
 
 app = Flask(__name__)
 app.secret_key = "thisisiskey"
 app.config["DEBUG"] = True
+app.database = "sample_sqlite3.db"
 
 
 @app.route('/')
@@ -14,6 +16,16 @@ def index():
     flash(str(rint))
     print(rint)
     return render_template("index.html", randint=str(rint))
+
+
+@app.route("/show_db")
+def show_db():
+    g.db = sqlite3.connect("sample_sqlite3.db")
+    cur = g.db.execute("select * from posts")
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    flash("showing db content in show_db page")
+    return render_template("show_db.html", posts=posts)
 
 
 if __name__ == "__main__":
---
+++ flask_proj/2_proj/sql.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+with sqlite3.connect("sample_sqlite3.db") as connection:
+    c = connection.cursor()
+    # """ and ' is the same function
+    c.execute("""CREATE TABLE posts(title TEXT, description TEXT)""")
+    c.execute('INSERT INTO posts VALUES("Good","I am good.")')
+    c.execute('INSERT INTO posts VALUES("Better","I am even better.")')
+
+'''
+run sqlite com from bash shell
+sqlite> .exit
+(flask_venv) Eddys-MBP:2_proj eddy$ sqlite3 sample_sqlite3_db 
+SQLite version 3.24.0 2018-06-04 14:10:15
+Enter ".help" for usage hints.
+sqlite> select * from posts;
+Good|I am good.
+Better|I am even better.
+sqlite> 
+'''
